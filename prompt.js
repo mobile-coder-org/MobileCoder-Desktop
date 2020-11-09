3dad54d6c5a0e0f3530dc56992f5345f9888305e
Alex <ah4597@nyu.edu>
2020-11-09 17:56:48 -0500

    added firebase authentication to sign-up and sign-in flows

--- prompt.js
+++ prompt.js
@@ -10,6 +10,22 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+var firebase = require('firebase/app');
+require("firebase/auth");
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDid2QqX_FubeW8euEBE-iMnWmLJCh5eqk",
+  authDomain: "mobilecoder-cf2ea.firebaseapp.com",
+  databaseURL: "https://mobilecoder-cf2ea.firebaseio.com",
+  projectId: "mobilecoder-cf2ea",
+  storageBucket: "mobilecoder-cf2ea.appspot.com",
+  messagingSenderId: "409368131515",
+  appId: "1:409368131515:web:ad9bb0eb12dd518cfb769e",
+  measurementId: "G-LSBZPKF91D"
+};
+
+
 
 /**
  * Beginning prompt function, takes user input and continuously loops (via. recursion) until a quit case is inputted.
@@ -22,33 +38,58 @@ const rl = readline.createInterface({
  *          TBD; Not yet implemented, likely to just be error handling asking user to enter a valid input.
  */
 function prompt(){
-        rl.question("What would you like to do? \n>", function(input){
-            switch(input){
-                case "quit":
-                case "Quit":
-                case "q":
-                case "Q":
-                    console.log("Goodbye!");
-                    rl.pause();
-                    break;
-                case "login":
-                case "Login":
-                    login(input);
-                    break;
-                default:
-                    console.log("Default case not yet implemented");
-                    prompt();
-            }
-        })
+    rl.resume();
+    rl.question("What would you like to do? \n>", function(input){
+        switch(input){
+            case "quit":
+            case "Quit":
+            case "q":
+            case "Q":
+                console.log("Goodbye!");
+                rl.pause();
+                break;
+            case "login":
+            case "Login":
+            case "l":
+            case "L":
+                login();
+                break;
+            case "signup":
+            case "Signup":
+            case "s":
+            case "S":
+                signup();
+                break;
+            default:
+                console.log("Default case not yet implemented");
+                prompt();
+        }
+    })
 }
 
-function login(input){
+/**
+ * Log in flow for 'login' case of prompt. Uses firebase Authentication to log user in.
+ */
+function login(){
     rl.question("Email: ", function(email){
         rl.question("Password: ", function(password){
-            console.log("Email: ", email, "Password: ", password, "\nAuthentication not yet implemented");
+            firebase.auth().signInWithEmailAndPassword(email, password);
             rl.pause();
         })
     })
 }
 
+/**
+ * Sign up flow for 'signup' case of prompt. Uses firebase Authentication to sign up a user.
+ */
+function signup(){
+    rl.question("Email: ", function(email){
+        rl.question("Password: ", function(password){
+            firebase.auth().createUserWithEmailAndPassword(email, password);
+            rl.pause();
+        })
+    })
+}
+
+firebase.initializeApp(firebaseConfig);
 prompt();
